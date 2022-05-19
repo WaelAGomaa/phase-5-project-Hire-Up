dd7327e9ca31db4bf66c8847fad645f6e1fbf9e7
wael <waeelash155@gmail.com>
2022-05-19 09:39:51 -0400

    sth

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+    has_secure_password
+    # messages relationship
+    has_many :messagee, foreign_key: :receiver_id, class_name: 'Message'  
+    has_many :senders, through: :messagee
+    has_many :messaged, foreign_key: :sender_id, class_name: 'Message'
+    has_many :receivers, through: :messaged
+
+    # User relationship "User is employee"
+    has_many :subordinates, class_name: "User", foreign_key: "employer_id"
+    belongs_to :employer, class_name: "User", optional: true
+
+    # User posts
+    has_many :posts
+
+    
+    validates :username, uniqueness: true
+    validates :username, :password_digest, :first_name, :last_name, presence: true
+
+end
